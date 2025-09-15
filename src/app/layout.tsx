8b3a2f73a0e2d97cdce6e54b130405787194d044
nicoloprog <nicosubywrx@gmail.com>
2025-09-15 12:56:54 -0400

    Seo keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,50 @@ import ChakraProviders from "@/contexts/ChakraProviders";
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "Concept Rénovation Prestige",
+  title:
+    "Concept Rénovation Prestige | Entrepreneur résidentielle et commerciale Rive-sud de Montréal",
   description:
-    "De la conception à la réalisation, nous construisons des espaces durables et sur mesure.",
+    "Experts en rénovation résidentielle et commerciale Rive-sud de Montréal : cuisine, salle de bain, sous-sol et projets extérieurs. Rive-sud",
+  // ✅ Add keywords
+  keywords: [
+    "rénovation Rive-sud de Montréal",
+    "constructiontion Rive-sud de Montréal",
+    "entrepreneur rénovation Rive-sud de Montréal",
+    "céramique Rive-sud de Montréal",
+    "cuisine céramique",
+    "construction extérieur maison",
+    "construction salle de bain, céramique",
+    "entrepreneur général Montréal",
+    "rénovation condo",
+    "rénovation commerciale Rive-sud de Montréal",
+    "rénovation résidentielle Rive-sud de Montréal",
+  ],
+
+  // ✅ Control search engine behavior
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  // ✅ Open Graph for social media sharing
+  openGraph: {
+    title:
+      "Concept Rénovation Prestige | Rénovation résidentielle et commerciale sur la Rive-sud de Montréal",
+    description:
+      "De la conception à la réalisation, nous créons des espaces durables et élégants à Montréal.",
+    url: "https://www.conceptrenovationprestige.com",
+    siteName: "Concept Rénovation Prestige",
+    images: [
+      {
+        url: "/crp.png",
+        width: 1200,
+        height: 630,
+        alt: "Rénovation haut de gamme Rive-sud de Montréal",
+      },
+    ],
+    locale: "fr_CA",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
